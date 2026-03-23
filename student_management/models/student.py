35acce3b2cbfd1d906ba8273c1e81f7f22412763
Long Duong <lduong@zensiontec.com>
2026-03-23 20:27:24 +0700

    Odoo ORM

--- student_management/models/student.py
+++ student_management/models/student.py
@@ -1,9 +1,28 @@
-from odoo import fields, models
-
+from odoo import fields, models, api
+from uuid import uuid4
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class Student(models.Model):
     _name = 'student.student'
 
     name = fields.Char(string='Name')
+    date_of_birth = fields.Date(string='Date of Birth')
+    age = fields.Integer()
     class_room_id = fields.Many2one(string='Class Room', comodel_name='student.classroom')
     subject_ids = fields.Many2many('subject.subject', string='Subjects')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        return super().create(vals_list)
+
+    def create_random_student(self):
+        self.create({
+            'name': str(uuid4())
+        })
+
+    @api.onchange('date_of_birth')
+    def _compute_age(self):
+        self.age = 0
+        if self.date_of_birth:
+            self.age = relativedelta(datetime.now().date(), self.date_of_birth).years
